39881ac535c4f9c079c46aa3039d6c52d6098292
andres-torres-2020 <andres.torres.192020@gmail.com>
2020-05-22 05:03:35 -0400

    Initial version.

---
+++ MovieRegistration/Controllers/MovieRegistrationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieRegistration.Models;
+
+namespace MovieRegistration.Controllers
+{
+    public class MovieRegistrationController : Controller
+    {
+        public IActionResult MovieRegistration()
+        {
+            return View();
+        }
+        public IActionResult Register(Movie movie)
+        //public IActionResult Register(int id, string title, string genre, int year, string actors, string directors)
+        {
+            if (ModelState.IsValid)
+            {
+
+                //Movie movie = new Movie();
+                //movie.ID = id;
+                //movie.Title = title;
+                //movie.Genre = genre;
+                //movie.Year = year;
+                //movie.Actors = actors;
+                //movie.Directors = directors;
+                return View("MovieRegistrationSummary", movie);
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ MovieRegistration/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieRegistration.Models
+{
+    public class Movie
+    {
+        [Required(ErrorMessage = "Please enter an integer for ID")]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a title with 50 characters max")]
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Please enter a date between 1880 and now")]
+        [Range(1880, 3000, ErrorMessage = "Year must be between 1880 and the current year")]
+        public int Year { get; set; }
+        public string Actors { get; set; }
+        public string Directors { get; set; }
+        //public List<string> Actors { get; set; } = new List<string>();
+        //public List<string> Directors { get; set; } = new List<string>();
+        public Movie()
+        {
+
+        }
+    }
+}
